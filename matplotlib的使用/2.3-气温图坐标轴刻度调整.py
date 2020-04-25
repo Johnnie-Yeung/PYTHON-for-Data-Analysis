9a5e89c8568617281881aa43c429f805162ce054
Johnnie-Yeung <64126666+Johnnie-Yeung@users.noreply.github.com>
2020-04-26 00:11:15 +0800

    Add files via upload

---
+++ matplotlib的使用/2.3-气温图坐标轴刻度调整.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+import random
+x = range(0,120)
+y = [random.randint(20,35) for i in range(0,120)]
+plt.figure(figsize=(20,8), dpi=60)
+plt.plot(x,y)
+
+# 调整x轴和y轴的刻度
+y_range = range(min(y),max(y))  # y的取值范围
+_xtick_labels = ['10:{}'.format(i) for i in range(60)] + ['11:{}'.format(i) for i in range(60)]
+_ytick_labels = ['{}℃'.format(i) for i in y_range]
+plt.xticks(list(x)[::5],_xtick_labels[::5],rotation=45)    # 第二个参数与第一个参数一一对应，可以让数字与字符串一一对应,rotation参数是x轴的刻度旋转的度数
+plt.yticks(y_range[::2],_ytick_labels[::2])
+
+plt.show()
