2d25f4c2b03eee748a8f9d8c54de0c7d256bfd6d
Johnnie-Yeung <64126666+Johnnie-Yeung@users.noreply.github.com>
2020-04-25 11:44:05 +0800

    Update 2.2-绘制每分钟气温变化的折线图.py

--- matplotlib的使用/2.2-绘制每分钟气温变化的折线图.py
+++ matplotlib的使用/2.2-绘制每分钟气温变化的折线图.py
@@ -4,9 +4,10 @@
 # coding = utf-8
 import random
 from matplotlib import pyplot as plt
-a = [random.randint(20, 35) for i in range(120)]
+x = range(0, 120)
+y = [random.randint(20, 35) for i in range(120)]
 plt.figure(figsize=(20, 8), dpi=60)
-plt.plot(a)
+plt.plot(x, y)
 plt.xticks(range(0, 121, 5))
 plt.yticks(range(20, 36))
 plt.show()
